80d30015a3c445f5318ac96258c5909a6825cd8f
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-11 19:01:34 +0530

    use of middlewares

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const express=require("express");
 const app=express();
 const PORT=8080;
+const {authAdmin}=require("./middleware/auth.js")
 
-app.use("/",(req,res,next)=>{
-    console.log("request accepted!");
-    next();
+// no need of auth 
+app.use("/user/login",(req,res)=>{
+    console.log("User logged in succesfull!");
+    res.send("Login sucessfull");
 });
 
-app.get("/",(req,res,next)=>{
-    console.log("Passing first request handler!");
-    next();
-},(req,res,next)=>{
-    console.log("presenet in second request handler!");
-    res.status(200).send("Data sent!");
-},(req,res,next)=>{
-    console.log("Present in third request handler!");
-});
+// for admin - one of of auth 
+// app.use("/admin",authAdmin);
 
+// or 
+app.get("/admin/getData",authAdmin,(req,res)=>{
+    console.log("Data sent for admin");
+    res.status(200).send("This is data!");
+});
 
 
 app.listen(PORT,()=>{
---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+//* dummy auth for admin route 
+const authAdmin=(req,res,next)=>{
+        const admin_token="xyz";
+        const user_token="xyz";
+        if(admin_token===user_token){
+            next();
+        }else{
+            res.send("Unauthorized access!");
+        }
+};
+
+module.exports={
+    authAdmin
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
