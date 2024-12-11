9f29a73f6843834e14d0492230529d17aca3c17f
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-11 17:02:49 +0530

    request handlers

--- src/app.js
+++ src/app.js
@@ -2,41 +2,15 @@ const express=require("express");
 const app=express();
 const PORT=8080;
 
-app.get("/user",(req,res)=>{
-    console.log(req.query);
-    res.status(200).send("data fetched!");
-});
-
-app.get("/ab?c",(req,res)=>{
-    res.status(200).send("working!");
-});
-
-
-app.get("/a(bc)?c",(req,res)=>{
-    res.status(200).send("working!");
-});
-
-
-app.get("/ab+c",(req,res)=>{
-    res.status(200).send("working multiple b!");
-});
-
-app.get("/ab*c",(req,res)=>{
-    res.status(200).send("working anything btw b and c!");
-});
-
-app.get(/a/,(req,res)=>{
-    res.status(200).send("find a ");
-});
-
-app.get(/.*fly$/,(req,res)=>{
-    res.status(200).send("ends with fly !");
-});
-
-app.get("/user/:id",(req,res)=>{
-    console.log(req.params);
-    res.status(200).send(req.params);
+app.use("/user",(req,res,next)=>{
+    console.log("Response 1!");
+    res.send("Response 1");
+    // next();
+},(req,res,next)=>{
+    console.log("Response 2!");
+    res.send("Response 2");
 })
+
 
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}`);
