e9a4ed7cb193408e06c15fceff5e2e504ab82ca8
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-11 12:55:12 +0530

    routing and regex with routing

--- src/app.js
+++ src/app.js
@@ -3,27 +3,40 @@ const app=express();
 const PORT=8080;
 
 app.get("/user",(req,res)=>{
-    console.log("Getting the user data!");
-    res.send(200,{"name":"Manoj"})
+    console.log(req.query);
+    res.status(200).send("data fetched!");
 });
 
-app.post("/user",(req,res)=>{
-    console.log("Posting data!");
-    res.send(200,"Posted data sucessfully!");
-});
-
-
-app.put("/user",(req,res)=>{
-    console.log("updated with put - data!");
-    res.send(200,"updated with put - data sucessfully!");
+app.get("/ab?c",(req,res)=>{
+    res.status(200).send("working!");
 });
 
 
-app.patch("/user",(req,res)=>{
-    console.log("Posting data!");
-    res.send(200,"patch - Posted data sucessfully!");
+app.get("/a(bc)?c",(req,res)=>{
+    res.status(200).send("working!");
 });
 
+
+app.get("/ab+c",(req,res)=>{
+    res.status(200).send("working multiple b!");
+});
+
+app.get("/ab*c",(req,res)=>{
+    res.status(200).send("working anything btw b and c!");
+});
+
+app.get(/a/,(req,res)=>{
+    res.status(200).send("find a ");
+});
+
+app.get(/.*fly$/,(req,res)=>{
+    res.status(200).send("ends with fly !");
+});
+
+app.get("/user/:id",(req,res)=>{
+    console.log(req.params);
+    res.status(200).send(req.params);
+})
 
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}`);
